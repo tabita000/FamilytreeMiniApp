5f9ad4e3b9a10dd5b98bfbab3e3977caa93e3138
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-02-12 18:48:43 -0600

    Initial Commit

---
+++ FamilytreeMiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilytreeMiniApp
+//
+//  Created by Tabita Sadiq on 2/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
